398ee0dbe7cf9dc946f3c0d918ebe7d8ff50a366
Александр Немашкало <avnemashkalo@miem.hse.ru>
2021-02-07 03:11:10 +0300

    наводим красоту на ноде часть 2

--- Back/SceneExportService/index.js
+++ Back/SceneExportService/index.js
@@ -1,11 +1,4 @@
 var express = require("express");
-
-const fs = require('fs');
-const path = require('path');
-const THREE = require('three');
-const program = require('commander');
-const Canvas = require('canvas');
-const { Blob, FileReader } = require('vblob');
 const routes = require('./routes/api.js')
 
 
@@ -23,51 +16,6 @@ app.use(express.urlencoded({
 }));
 
 app.use(routes.router)
-
-app.post("/gapi/gltf", (req, res) => {
-
-
-    global.window = global;
-    global.Blob = Blob;
-    global.FileReader = FileReader;
-    global.THREE = THREE;
-    global.document = {
-        createElement: (nodeName) => {
-            if (nodeName !== 'canvas') throw new Error(`Cannot create node ${nodeName}`);
-            const canvas = new Canvas(256, 256);
-            // This isn't working — currently need to avoid toBlob(), so export to embedded .gltf not .glb.
-            // canvas.toBlob = function () {
-            //   return new Blob([this.toBuffer()]);
-            // };
-            return canvas;
-        }
-    };
-
-    require('three/examples/js/exporters/GLTFExporter');
-
-
-    const fileName =  'example.gltf'
-
-    var loader = new THREE.ObjectLoader();
-
-    loader.parse(req.body, ( resultScene ) => {
-
-        console.log('Converting to glTF');
-        const exporter = new THREE.GLTFExporter();
-        exporter.parse(resultScene, (content) => {
-
-            console.log(`Writing to ${fileName}`);
-            if (typeof content === 'object') content = JSON.stringify(content);
-            fs.writeFileSync(fileName, content);
-
-        });
-    })
-
-
-    res.status(200).json({message: 'Заебись все работает'})
-});
-
-
 
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`)
--- Back/SceneExportService/routes/api.js
+++ Back/SceneExportService/routes/api.js
@@ -1,7 +1,5 @@
 var express = require('express')
 var methods = require('../controllers/gltf.js')
-
-
 var router = express.Router()
 
 router.post('/gapi/gltf', methods.create)
