7a95724b909836b40f93b13e2dc0420c2ce32c0b
Александр Немашкало <avnemashkalo@miem.hse.ru>
2021-02-07 03:13:25 +0300

    наводим красоту на ноде часть 3

--- Back/SceneExportService/routes/api.js
+++ Back/SceneExportService/routes/api.js
@@ -2,7 +2,6 @@ var express = require('express')
 var methods = require('../controllers/gltf.js')
 var router = express.Router()
 
-router.post('/gapi/gltf', methods.create)
-
+router.post('/exporter/gltf', methods.create)
 
 module.exports.router = router
