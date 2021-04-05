80ae82ac0d9705c84b8b266091fa77bdc5d607b7
IvanKalimanov <ikalimanov@mail.ru>
2021-04-05 22:47:40 +0300

    SceneExportService docker files

--- Back/docker-commands.ps1
+++ Back/docker-commands.ps1
@@ -1,5 +1,6 @@
 docker build -t webapi -f hsear/hsear.scanner.api/Dockerfile .
 docker build -t webapi -f hsear/hsear.scanner.api/Dockerfile .
+docker build -t sceneexportservice -f SceneExportService/Dockerfile .
 docker network create --driver bridge hsear_network
 cd  HseAr
 docker compose up -d
