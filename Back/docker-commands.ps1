52d5f864bbb49f6d08c56162284e496a85012168
IvanKalimanov <ikalimanov@mail.ru>
2021-04-13 20:30:18 +0300

    Front Dockerfile added

--- Back/docker-commands.ps1
+++
@@ -1,6 +0,0 @@
-docker build -t webapi -f hsear/hsear.scanner.api/Dockerfile .
-docker build -t webapi -f hsear/hsear.webplatform.api/Dockerfile .
-docker build -t sceneexportservice -f SceneExportService/Dockerfile .
-docker network create --driver bridge hsear_network
-cd  HseAr
-docker compose up -d
