bdbf419836eb5f4a1b504c8481b9008b91439c6d
tikhusaenov <50641103+tikhusaenov@users.noreply.github.com>
2021-04-20 02:10:39 +0300

    верстка админки

---
+++ FrontWeb/front/src/components/app/ModalWindow.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+    export default {
+        name: "ModalWindow"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ FrontWeb/front/src/views/Buildings/BuildingsSlider.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+    export default {
+        name: "BuildingsSlider"
+    }
+</script>
+
+<style scoped>
+
+</style>
