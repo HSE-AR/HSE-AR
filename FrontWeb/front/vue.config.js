37cdd1ff28ff9ae95e77e186bb488811f812a525
IvanKalimanov <ikalimanov@mail.ru>
2021-04-24 16:11:16 +0300

    fix

--- FrontWeb/front/vue.config.js
+++ FrontWeb/front/vue.config.js
@@ -1,10 +1,12 @@
+const fs = require('fs');
+
 module.exports = {
     devServer: {
       open: process.platform === 'darwin',
       host: '0.0.0.0',
       port: 5000, // CHANGE YOUR PORT HERE!
-      key: fs.readFileSync('./hsear.ru.key', 'utf8'),
-      cert: fs.readFileSync('./hsear.ru.cert', 'utf8'),
+      key: fs.readFileSync('./localhost.key', 'utf8'),
+      cert: fs.readFileSync('./localhost.crt', 'utf8'),
       https: true,
       hotOnly: false,
     },
